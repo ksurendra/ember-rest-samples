1a7d27012957fc2a27d9a1bb721740ee54b7625f
Charles Fries <charliefries@icloud.com>
2022-02-14 13:26:57 -0800

    Remove `id` attr

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,6 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class UserModel extends Model {
-  @attr('string') id;
   @attr('string') firstname;
   @attr('string') lastname;
   @attr('string') email;
